0da14e426cb1e20e8e60f29469860b1612da8aa8
Antonija <ipekica@gmail.com>
2018-04-30 20:58:34 +0300

    New Facebook login

--- Lez/Controllers/RegisterViewController.swift
+++ Lez/Controllers/RegisterViewController.swift
@@ -12,9 +12,9 @@ import FacebookLogin
 import Firebase
 import JGProgressHUD
 
-class RegisterViewController: UIViewController, FBSDKLoginButtonDelegate {
+class RegisterViewController: UIViewController {
     
-    var loginButton: FBSDKLoginButton!
+    let facebookLoginButton = UIButton()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -28,51 +28,102 @@ class RegisterViewController: UIViewController, FBSDKLoginButtonDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        loginButton = FBSDKLoginButton()
-        loginButton.readPermissions = ["public_profile", "email"]
-        loginButton.delegate = self
-        view.addSubview(loginButton)
-        loginButton.snp.makeConstraints { (make) in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview().inset(90)
+        setupLoginButter()
+//        loginButton = FBSDKLoginButton()
+//        loginButton.readPermissions = ["public_profile", "email"]
+//        loginButton.delegate = self
+//        view.addSubview(loginButton)
+//        loginButton.snp.makeConstraints { (make) in
+//            make.centerX.equalToSuperview()
+//            make.centerY.equalToSuperview().inset(90)
+//        }
+    }
+    
+    func setupLoginButter() {
+        // Add a custom login button to your app
+        facebookLoginButton.backgroundColor = .blue
+        facebookLoginButton.setTitle("Login with Facebook", for: .normal)
+        
+        // Handle clicks on the button
+        facebookLoginButton.addTarget(self, action: #selector(self.facebookButtonTapped), for:.touchUpInside)
+        
+        // Add the button to the view
+        view.addSubview(facebookLoginButton)
+        facebookLoginButton.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview().inset(48)
+            make.bottom.equalToSuperview().inset(100)
         }
     }
     
-    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
-        
-    }
-    
-    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
-        if let error = error {
-            print(error.localizedDescription)
-            return
-        }
-        // Start Animating
-        let hud = JGProgressHUD(style: .dark)
-        hud.textLabel.text = "Please wait"
-        hud.vibrancyEnabled = true
-        hud.show(in: view)
-        
-        // Hide Button
-        loginButton.isHidden = true
-        
-        let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
-        Auth.auth().signIn(with: credential) { (user, error) in
-            if let error = error {
+    @objc func facebookButtonTapped() {
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [.email, .publicProfile], viewController: self) { (result) in
+            switch result {
+            case .failed(let error):
                 print(error)
-                return
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(_, _, let accessToken):
+                
+                print("Logged in!")
+                let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
+                Auth.auth().signIn(with: credential) { (user, error) in
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    // User is signed in
+                    guard let current = user else { return }
+
+                    FirestoreManager.shared.fetchUser(uid: current.uid).then { (user) in
+                        print(user.name)
+                        print(user.isOnboarded)
+                        if user.isOnboarded {
+                            self.dismiss(animated: true, completion: nil)
+                        } else {
+                            let setupProfileViewController = SetupProfileViewController()
+                            setupProfileViewController.name = current.displayName!
+                            setupProfileViewController.email = current.email!
+                            setupProfileViewController.uid = current.uid
+                            self.navigationController?.pushViewController(setupProfileViewController, animated: true)
+                            self.navigationItem.setHidesBackButton(true, animated: true)
+                        }
+                    }
+                }
             }
-            // User is signed in
-            print(user!)
-            guard let usr = user else { return }
-            let setupProfileViewController = SetupProfileViewController()
-            setupProfileViewController.name = usr.displayName!
-            setupProfileViewController.email = usr.email!
-            setupProfileViewController.uid = usr.uid
-            hud.dismiss(animated: true)
-            self.navigationController?.pushViewController(setupProfileViewController, animated: true)
-            self.navigationItem.setHidesBackButton(true, animated: true)
         }
     }
+    
+//    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+//        if let error = error {
+//            print(error.localizedDescription)
+//            return
+//        }
+//        // Start Animating
+//        let hud = JGProgressHUD(style: .dark)
+//        hud.textLabel.text = "Please wait"
+//        hud.vibrancyEnabled = true
+//        hud.show(in: view)
+//
+//        // Hide Button
+//        loginButton.isHidden = true
+//
+//        let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+//        Auth.auth().signIn(with: credential) { (user, error) in
+//            if let error = error {
+//                print(error)
+//                return
+//            }
+//            // User is signed in
+//            print(user!)
+//            guard let usr = user else { return }
+//            let setupProfileViewController = SetupProfileViewController()
+//            setupProfileViewController.name = usr.displayName!
+//            setupProfileViewController.email = usr.email!
+//            setupProfileViewController.uid = usr.uid
+//            hud.dismiss(animated: true)
+//            self.navigationController?.pushViewController(setupProfileViewController, animated: true)
+//            self.navigationItem.setHidesBackButton(true, animated: true)
+//        }
+//    }
 }
